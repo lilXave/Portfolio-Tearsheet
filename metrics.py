14d2017046ae3be9893c72d28e3511e53770d38a
Xavier Aslan Guenaydin <xaviergunaydin@gmail.com>
2026-08-20 15:06:38 +0200

    #beta, alpha täglich, r², info ratio für gesamten zeitraum
    #beta, alpha täglich, r², info ratio

--- metrics.py
+++ metrics.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import math
-
+# Konstanten
 RISK_FREE_RATE = 0.05 #ann%
 TRADING_DAYS = 252
 
@@ -31,6 +31,8 @@ def get_cgar(arr: np.ndarray) -> float:
     t_ = arr.size / TRADING_DAYS
     total_ret = np.sum(arr) 
     return np.exp(total_ret / t_) - 1
+def get_calmar(cgar: float, max_dd: float):
+    return cgar/max_dd
 
 def get_beta(asset: np.ndarray, benchmark:np.ndarray):
     cov_mat = np.cov(m=asset,y=benchmark)
@@ -44,27 +46,36 @@ def get_alpha(beta: float, asset: np.ndarray, benchmark:np.ndarray) -> float:
     eb = benchmark_ret - rf
     return ea -beta*eb
 
+def get_r_pow_2(asset: np.ndarray, benchmark:np.ndarray) -> tuple[float, float, float, float]:#beta, alpha täglich, r², info ratio
+    
+    rf = np.log(1.05) / TRADING_DAYS
+    ea = asset - rf
+    eb = benchmark - rf
+    cov_mat = np.cov(m=ea,y=eb)
+    beta = cov_mat[0,1]/cov_mat[1,1]
+    alpha = (ea.mean() -beta*eb.mean())
+    y_hat = alpha + beta * eb
+    epsilon = ea - y_hat
+    r_2 = 1 - epsilon.var() / ea.var()
+    information_ratio = (alpha / epsilon.std()) * np.sqrt(TRADING_DAYS)
+    return (beta, alpha, r_2, information_ratio)
+
 def get_all_metrics(df: pd.DataFrame):
     asset_ret_arr = df['asset_ret'].to_numpy()
     benchmark_ret_arr = df['benchmark_ret'].to_numpy()
     asset_max_dd = get_max_dd(asset_ret_arr)
     benchmark_max_dd = get_max_dd(benchmark_ret_arr)
     ann_asset_vol = get_ann_vol(asset_ret_arr)
+    cgar = get_cgar(asset_ret_arr)
+    calmar = get_calmar(cgar, asset_max_dd)
     asset_sharp = get_sharp_ratio (asset_ret_arr)
-    asset_sortino = get_sortino_ratio(asset_ret_arr)
-    asset_cgar = get_cgar(asset_ret_arr)
-    asset_ref_beta = get_beta(asset_ret_arr, benchmark_ret_arr)
-    asset_alpha = get_alpha(asset_ref_beta, asset_ret_arr, benchmark_ret_arr)
+    asset_sortino = get_sortino_ratio(asset_ret_arr)    
+    beta, alpha, r_2, information_ratio = get_r_pow_2(asset_ret_arr, benchmark_ret_arr)
 
-
-    print (asset_alpha)
-    print (asset_ref_beta)
-    print (asset_cgar)
-    print (ann_asset_vol)
-    print (asset_max_dd)
-    print (asset_sharp)
-    print (asset_sortino)
-
+    print (beta)
+    print (alpha)
+    print (r_2)
+    print (information_ratio)
 if __name__ == '__main__':
     df = pd.read_csv("out.csv")
     get_all_metrics(df)
--- return_to_benchmark.py
+++ return_to_benchmark.py
@@ -5,8 +5,8 @@ import numpy as np
 
 def get_csv_data(start_date : str, benchmark : str, asset : str) -> pd.DataFrame :
     INTERVAL = "1d"
-    asset_data = yf.Ticker(asset).history(start=start_date, interval=INTERVAL)
-    benchmark_data = yf.Ticker(benchmark).history(start=start_date, interval=INTERVAL)
+    asset_data = yf.Ticker(asset).history(start=start_date, interval=INTERVAL, auto_adjust=True)
+    benchmark_data = yf.Ticker(benchmark).history(start=start_date, interval=INTERVAL, auto_adjust=True)
     asset_log_ret = np.log(asset_data['Close']).diff()
     asset_log_ret = asset_log_ret[~np.isnan(asset_log_ret)]
     benchmark_log_ret = np.log(benchmark_data['Close']).diff()
